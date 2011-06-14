d81a01f9b6dfa2e34bfc434537a06d16b77d6920
i.plosker <i.plosker@cipher-sys.com>
2011-06-14 11:13:43 -0400

    Using records to clarify and simplify code (?).

--- src/seq.erl
+++ src/seq.erl
@@ -3,18 +3,22 @@
 
 -type seq(T) :: { seq, fun(() -> T), fun(() -> seq(T)) }.
 
+-record(seq, {
+    first :: fun(() -> T),
+    rest :: fun(() -> T)
+}).
+
 -spec first(seq(T)) -> T.
-first({ seq, First, _ }) -> First().
+first(Seq) -> (Seq#seq.first)().
 
 -spec rest(seq(T)) -> seq(T) | T.
-rest({ seq, _, Rest }) -> Rest().
+rest(Seq) -> (Seq#seq.rest)().
 
 -spec cons(T, seq(T)) -> seq(T).
 cons(N, Seq) ->
-    {
-        seq,
-        fun() -> N end,
-        fun() -> Seq end
+    #seq{
+        first = fun() -> N end,
+        rest = fun() -> Seq end
     }.
 
 -spec nth(integer(), seq(T)) -> T.
@@ -27,10 +31,9 @@ nthrest(N, Seq) when N > 1 -> nth(N - 1, rest(Seq)).
 
 -spec from_list([T,...]) -> seq(T).
 from_list([H | T]) -> 
-    {
-        seq,
-        fun() -> H end,
-        case T of
+    #seq{
+        first = fun() -> H end,
+        rest = case T of
             [] -> fun() -> undefined end;
             _  -> fun() -> from_list(T) end
         end
@@ -38,59 +41,55 @@ from_list([H | T]) ->
 
 -spec series(integer(), integer()) -> seq(integer()).
 series(Start, Interval) ->
-    {
-        seq,
-        fun() -> Start end,
-        fun() -> series(Start + Interval, Interval) end
+    #seq{
+        first = fun() -> Start end,
+        rest = fun() -> series(Start + Interval, Interval) end
     }.
 
 -spec random_integers(integer(), integer()) -> seq(integer()).
 random_integers(Minimum, Maximum) when Minimum < Maximum ->
     RandomInteger = random:uniform(Maximum - Minimum + 1),
-    {
-        seq,
-        fun() -> RandomInteger + Minimum - 1 end,
-        fun() -> random_integers(Minimum, Maximum) end
+    #seq{
+        first = fun() -> RandomInteger + Minimum - 1 end,
+        rest = fun() -> random_integers(Minimum, Maximum) end
     }.
 
 -spec to_list(seq(T) | [] | T) -> [T].
-to_list({ seq, First, Rest }) -> [ First() | to_list(Rest()) ];
-to_list(undefined) -> [].
+to_list(undefined) -> [];
+to_list(Seq) -> [ first(Seq) | to_list(rest(Seq)) ].
 
 -spec map(fun((T) -> U), seq(T)) -> seq(U).
 map(_, undefined) -> undefined;
-map(Mapper, { seq, Head, Rest }) ->
-    {
-        seq,
-        fun() -> Mapper(Head()) end,
-        fun() -> map(Mapper, Rest()) end
+map(Mapper, Seq) ->
+    #seq{
+        first = fun() -> Mapper(first(Seq)) end,
+        rest = fun() -> map(Mapper, rest(Seq)) end
     }.
 
 -spec fold(fun((T, any()) -> any()), any(), seq(T)) -> any().
 fold(_, Acc, undefined) -> Acc;
-fold(Fn, Acc, { seq, Head, Rest }) -> fold(Fn, Fn(Head(), Acc), Rest()).
+fold(Fn, Acc, Seq) -> fold(Fn, Fn(first(Seq), Acc), rest(Seq)).
 
 
 -spec filter(fun((T) -> boolean()), seq(T)) -> seq(T).
 filter(_, undefined) -> undefined;
-filter(Filter, { seq, First, Rest }) ->
-    {
-        seq,
-        fun() ->
-            case Filter(First()) of
-                true -> First();
+filter(Filter, Seq) ->
+    #seq{
+        first = fun() ->
+            case Filter(first(Seq)) of
+                true -> first(Seq);
                 false -> 
-                    case (FRest = filter(Filter, Rest())) /= undefined of
+                    case (FRest = filter(Filter, rest(Seq))) /= undefined of
                         true -> first(FRest);
                         false -> undefined
                     end
             end
         end,
-        fun() ->
-            case Filter(First()) of
-                true -> filter(Filter, Rest());
+        rest = fun() ->
+            case Filter(first(Seq)) of
+                true -> filter(Filter, rest(Seq));
                 false ->
-                    case (FRest = filter(Filter, Rest())) /= undefined of
+                    case (FRest = filter(Filter, rest(Seq))) /= undefined of
                         true -> rest(FRest);
                         false -> undefined
                     end
@@ -101,21 +100,19 @@ filter(Filter, { seq, First, Rest }) ->
 -spec take(pos_integer(), seq(T)) -> seq(T) | T.
 take(_, undefined) -> undefined;
 take(0, _) -> undefined;
-take(C, {seq, First, Rest }) ->
-    {
-        seq,
-        fun() -> First() end,
-        fun() -> take(C - 1, Rest()) end
+take(C, Seq) ->
+    #seq{
+        first = fun() -> first(Seq) end,
+        rest = fun() -> take(C - 1, rest(Seq)) end
     }.
 
 -spec zip(fun((T, U) -> V), seq(T), seq(U)) -> seq(V).
 zip(_Zipper, _Seq1, undefined) -> undefined;
 zip(_Zipper, undefined, _Seq2) -> undefined;
 zip(Zipper, Seq1, Seq2) ->
-    {
-        seq,
-        fun() -> Zipper(first(Seq1), first(Seq2)) end,
-        fun() -> zip(Zipper, rest(Seq1), rest(Seq2)) end
+    #seq{
+        first = fun() -> Zipper(first(Seq1), first(Seq2)) end,
+        rest = fun() -> zip(Zipper, rest(Seq1), rest(Seq2)) end
     }.
 
 -spec zip3(fun((T, U, V) -> W), seq(T), seq(U), seq(V)) -> seq(W).
@@ -123,8 +120,7 @@ zip3(_Zipper, _Seq1, _Seq2, undefined) -> undefined;
 zip3(_Zipper, _Seq1, undefined, _Seq3) -> undefined;
 zip3(_Zipper, undefined, _Seq2, _Seq3) -> undefined;
 zip3(Zipper, Seq1, Seq2, Seq3) ->
-    {
-        seq,
-        fun() -> Zipper(first(Seq1), first(Seq2), first(Seq3)) end,
-        fun() -> zip3(Zipper, rest(Seq1), rest(Seq2), rest(Seq3)) end
+    #seq{
+        first = fun() -> Zipper(first(Seq1), first(Seq2), first(Seq3)) end,
+        rest = fun() -> zip3(Zipper, rest(Seq1), rest(Seq2), rest(Seq3)) end
     }.
